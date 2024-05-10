4e1fb5a902f029b1dcee2e402afab41d6454e5fa
dumbled00r <kazuu.promotion@gmail.com>
2024-05-10 08:27:17 +0700

    add search global layout

--- forum/components/shared/navbar/Navbar.tsx
+++ forum/components/shared/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import GlobalSearch from "../search/GlobalSearch";
 import MobileNav from "./MobileNav";
 import Theme from "./Theme";
 
@@ -18,7 +19,7 @@ const Navbar = () => {
           BuFFer<span className="text-primary-500">Overflow</span>
         </p>
       </Link>
-      Global search
+      <GlobalSearch />
       <div className="flex-between gap-5">
         <Theme></Theme>
         <SignedIn>
