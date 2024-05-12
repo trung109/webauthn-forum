6d37a1df6984bd51d27e7570ce31200f55f4ea92
dumbled00r <kazuu.promotion@gmail.com>
2024-05-12 22:18:16 +0700

    add sidebars

--- forum/components/shared/LeftSideBar.tsx
+++ forum/components/shared/LeftSideBar.tsx
@@ -8,7 +8,7 @@ import { Button } from "../ui/button";
 const LeftSideBar = () => {
   const pathname = usePathname();
   return (
-    <section className="background-light900_dark200 light-border max-sm: custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none lg:w-[266px]">
+    <section className="background-light900_dark200 light-border custom-scrollbar no-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
