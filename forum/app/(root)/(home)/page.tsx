e8221fe172c294eb090f4f0d312a965279f2dbd6
dumbled00r <kazuu.promotion@gmail.com>
2024-05-13 22:34:44 +0700

    add home filters

--- forum/app/(root)/(home)/page.tsx
+++ forum/app/(root)/(home)/page.tsx
@@ -1,3 +1,37 @@
+import HomeFilter from "@/components/home/HomeFilter";
+import Filter from "@/components/shared/Filter";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Posts</h1>
+        <Link href="/create-post" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Create a post
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for posts"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilter />
+    </>
+  );
 }
