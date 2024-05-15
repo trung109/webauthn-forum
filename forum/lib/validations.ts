17c154847c7515b294a5b31d3c7629495250a219
HanselGray <124652238+HanselGray@users.noreply.github.com>
2024-05-15 22:36:09 +0700

    change login format

--- forum/lib/validations.ts
+++ forum/lib/validations.ts
@@ -25,15 +25,13 @@ export const signupFormSchema = z.object({
 });
 
 export const signinFormSchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  username: z
+    .string()
+    .regex(/^(?!.*@).*/, { message: "Username cannot contain the @ symbol." })
+    .min(2, { message: "Username must be at least 2 characters long." })
+    .trim(),
   password: z
     .string()
-    .min(8, { message: "Be at least 8 characters long" })
-    .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
-    .regex(/[0-9]/, { message: "Contain at least one number." })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "Contain at least one special character.",
-    })
     .trim(),
 });
 
