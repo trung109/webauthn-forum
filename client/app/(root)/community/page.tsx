d3f6a566325fa9feeb7c38635da0dace3360e815
dumbled00r <82684246+dumbled00r@users.noreply.github.com>
2024-05-30 23:06:36 +0700

    Add create post button

--- client/app/(root)/community/page.tsx
+++ client/app/(root)/community/page.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      
+    <>
+    <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+      <h1 className='h1-bold text-dark100_light900'>All Posts</h1>
+
     </div>
+    </>
   )
 }
 
