af340db7957d06d9d1cfe6e2b7ce2fdb5b46fb95
dumbled00r <82684246+dumbled00r@users.noreply.github.com>
2024-06-02 22:42:48 +0700

    change self role

--- client/helper/components/cards/UserHorizontalCard.tsx
+++ client/helper/components/cards/UserHorizontalCard.tsx
@@ -15,6 +15,7 @@ import {
   DropdownMenuTrigger
 } from '@/helper/components/ui/dropdown-menu';
 import { useUser } from '@/app/context/UserContext';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   username: string;
@@ -38,6 +39,7 @@ const UserHorizontalCard = ({
   joinedAt
 }: Props) => {
   const { user } = useUser();
+  const router = useRouter();
   const [roleSet, setRoleSet] = useState<string>(role);
 
   const handleChangeRole = async (newRole: string) => {
@@ -61,77 +63,81 @@ const UserHorizontalCard = ({
       console.log(error);
     } finally {
       if (username === user.username) {
-        window.location.reload();
+        router.push('/home');
       }
     }
   };
 
   return (
-    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
-        <div className="">
-          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-            {/* {getTimestamp(createdAt)} */}
-          </span>
-          <Link href={`/user?userId=${id}`}>
-            <div className="flex flex-row gap-6 items-center">
-              <Image
-                src={photoUrl}
-                width={75}
-                height={75}
-                alt="profile picture"
-                className="object-cover rounded-full"
-              />
-              <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
-                @{username}
-              </h3>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center">
-          <Metric
-            imgUrl="/assets/icons/email.svg"
-            alt="email icon"
-            value={email}
-            title=""
-            textStyles="text-dark400_light800"
-          ></Metric>
-          <Metric
-            imgUrl="/assets/icons/clock.svg"
-            alt="clock icon"
-            value={`Joined at: ${
-              joinedAt ? getTimestamp(joinedAt) : 'May 2024'
-            }`}
-            title=""
-            textStyles="text-dark400_light800"
-          ></Metric>
-        </div>
-        <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="w-20">
-                {roleSet[0].toUpperCase() + roleSet.slice(1)}
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-20">
-              <DropdownMenuSeparator />
-              <DropdownMenuRadioGroup
-                value={roleSet}
-                onValueChange={handleChangeRole}
-              >
-                <DropdownMenuRadioItem value="admin">
-                  Admin
-                </DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="user">User</DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="banned">
-                  Banned
-                </DropdownMenuRadioItem>
-              </DropdownMenuRadioGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+    <>
+      <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+        <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+          <div className="">
+            <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+              {/* {getTimestamp(createdAt)} */}
+            </span>
+            <Link href={`/user?userId=${id}`}>
+              <div className="flex flex-row gap-6 items-center">
+                <Image
+                  src={photoUrl}
+                  width={75}
+                  height={75}
+                  alt="profile picture"
+                  className="object-cover rounded-full"
+                />
+                <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+                  @{username}
+                </h3>
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <Metric
+              imgUrl="/assets/icons/email.svg"
+              alt="email icon"
+              value={email}
+              title=""
+              textStyles="text-dark400_light800"
+            ></Metric>
+            <Metric
+              imgUrl="/assets/icons/clock.svg"
+              alt="clock icon"
+              value={`Joined at: ${
+                joinedAt ? getTimestamp(joinedAt) : 'May 2024'
+              }`}
+              title=""
+              textStyles="text-dark400_light800"
+            ></Metric>
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="w-20">
+                  {roleSet[0].toUpperCase() + roleSet.slice(1)}
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-20">
+                <DropdownMenuSeparator />
+                <DropdownMenuRadioGroup
+                  value={roleSet}
+                  onValueChange={handleChangeRole}
+                >
+                  <DropdownMenuRadioItem value="admin">
+                    Admin
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="user">
+                    User
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="banned">
+                    Banned
+                  </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
