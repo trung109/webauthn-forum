6e9113278ccab638199feb913a69548c0f1b55bb
HanselGray <124652238+HanselGray@users.noreply.github.com>
2024-06-03 02:18:24 +0700

    add: anti dos, brute

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,7 +2,19 @@ import express from 'express'
 import { createPost, getPosts, getPostById, getPendingPosts, approvePost, declinePost, getPostComments, searchPosts } from '../controllers/post.js'
 import { requireSignIn , requireAdmin} from '../middlewares/index.js'
 
+import { rateLimit } from 'express-rate-limit'
+
+const authLimiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 15 minutes
+	limit: 15, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    statusCode: 404,
+	message: 'Too many request, try again later'
+})
+
 const router = express.Router()
+
+router.use(authLimiter)
 
 router.post('/post', requireSignIn,createPost)
 
